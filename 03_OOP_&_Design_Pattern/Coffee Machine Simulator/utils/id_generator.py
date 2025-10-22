ec10f98be93d24b9d7b608a91d313883b56e33f2
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-22 09:42:47 -0700

    Update id_generator.py

--- 03_OOP_&_Design_Pattern/Coffee Machine Simulator/utils/id_generator.py
+++ 03_OOP_&_Design_Pattern/Coffee Machine Simulator/utils/id_generator.py
@@ -1,1 +1,13 @@
+"""
+Module: id_generator.py
+-----------------------
+Provides utility function for generating unique order or transaction IDs.
+"""
+
+import random, string
+
+def generate_id(prefix: str) -> str:
+    """Generate a unique alphanumeric ID."""
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    return f"{prefix}-{code}"
 
