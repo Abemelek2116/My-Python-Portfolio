d93c35834b10a5cba41051637c8bf522c4f475b3
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-22 09:41:11 -0700

    Update recipe.py

--- 03_OOP_&_Design_Pattern/Coffee Machine Simulator/models/recipe.py
+++ 03_OOP_&_Design_Pattern/Coffee Machine Simulator/models/recipe.py
@@ -1,1 +1,23 @@
+"""
+Module: recipe.py
+-----------------
+Defines the Recipe class that outlines ingredient requirements for a specific drink.
+"""
+
+class Recipe:
+    """
+    Represents a drink recipe composed of ingredient requirements.
+    """
+
+    def __init__(self, ingredients: dict):
+        """
+        Initialize a recipe with a mapping of ingredient names to quantities.
+
+        :param ingredients: Dictionary of {ingredient_name: amount_required}
+        """
+        self.ingredients = ingredients
+
+    def get_ingredients(self):
+        """Return the ingredient requirements."""
+        return self.ingredients
 
