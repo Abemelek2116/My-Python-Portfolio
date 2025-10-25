3a9955b62d49fc1068ced366ab929f884f4276f0
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-25 10:15:17 -0700

    Update observer.py

--- 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/observer.py
+++ 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/observer.py
@@ -1,1 +1,26 @@
 
+"""
+observer.py
+-------------
+Implements the Observer design pattern.
+Used for sending notifications (e.g., low stock alerts) when inventory changes.
+"""
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer):
+        """Attach a new observer."""
+        self._observers.append(observer)
+
+    def notify(self, message):
+        """Notify all observers."""
+        for observer in self._observers:
+            observer.update(message)
+
+
+class Observer:
+    def update(self, message):
+        """Method to be implemented by concrete observers."""
+        raise NotImplementedError
