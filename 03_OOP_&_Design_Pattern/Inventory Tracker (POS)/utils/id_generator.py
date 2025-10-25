871daafd321344a1032bf1e3ccbff79a2a983680
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-25 10:15:00 -0700

    Update id_generator.py

--- 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/id_generator.py
+++ 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/id_generator.py
@@ -1,1 +1,14 @@
+"""
+id_generator.py
+----------------
+Utility class for generating unique IDs for products and sales.
+"""
+
+import uuid
+
+class IDGenerator:
+    @staticmethod
+    def generate_id():
+        """Return a unique ID."""
+        return str(uuid.uuid4())[:8]
 
