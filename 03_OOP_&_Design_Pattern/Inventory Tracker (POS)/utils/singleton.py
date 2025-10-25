8b72ea2b79b1e6b7302b3c901c3e7ee2df48881d
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-25 10:14:37 -0700

    Update singleton.py

--- 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/singleton.py
+++ 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/utils/singleton.py
@@ -1,1 +1,17 @@
 
+"""
+singleton.py
+--------------
+Implements the Singleton design pattern.
+Used to ensure classes like Inventory have only one instance throughout the app.
+"""
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """Return existing instance if present, else create one."""
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
