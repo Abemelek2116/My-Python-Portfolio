d2e1aced450562efe6604a20dfd2822a0c76365b
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-25 10:16:55 -0700

    Update inventory_service.py

--- 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/services/inventory_service.py
+++ 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/services/inventory_service.py
@@ -1,1 +1,19 @@
+"""
+inventory_service.py
+---------------------
+Provides high-level methods for managing inventory operations.
+"""
+
+from models.inventory import Inventory
+
+class InventoryService:
+    def __init__(self):
+        self.inventory = Inventory()
+
+    def add_new_product(self, name, price, quantity):
+        return self.inventory.add_product(name, price, quantity)
+
+    def display_inventory(self):
+        for product in self.inventory.list_products():
+            print(product)
 
