ba8defdf1400d1f94fbd42137b56749b8a1d0dea
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-25 10:23:43 -0700

    Update report_service.py

--- 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/services/report_service.py
+++ 03_OOP_&_Design_Pattern/Inventory Tracker (POS)/services/report_service.py
@@ -1,1 +1,17 @@
+"""
+report_service.py
+------------------
+Generates basic reports for sales and inventory performance.
+"""
+
+class ReportService:
+    def __init__(self, inventory_service, sale_service):
+        self.inventory_service = inventory_service
+        self.sale_service = sale_service
+
+    def generate_report(self):
+        print("\n=== 🧾 DAILY SALES REPORT ===")
+        self.sale_service.show_sales()
+        print("\n=== 📦 CURRENT INVENTORY ===")
+        self.inventory_service.display_inventory()
 
