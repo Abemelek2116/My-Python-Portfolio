17c109e3ad32dd15e704cade9ff527743e538300
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 22:46:45 -0700

    Update observer.py

--- 03_OOP_&_Design_Pattern/Library Management System/utils/observer.py
+++ 03_OOP_&_Design_Pattern/Library Management System/utils/observer.py
@@ -1,1 +1,26 @@
+"""
+observer.py
+-------------
+Implements the Observer design pattern.
+Used for sending notifications to members when due dates are reached or other events occur.
+"""
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer):
+        """Attach an observer (like a notification service)."""
+        self._observers.append(observer)
+
+    def notify(self, message):
+        """Send a message to all observers."""
+        for observer in self._observers:
+            observer.update(message)
+
+
+class Observer:
+    def update(self, message):
+        """Abstract method to handle notifications."""
+        raise NotImplementedError
 
