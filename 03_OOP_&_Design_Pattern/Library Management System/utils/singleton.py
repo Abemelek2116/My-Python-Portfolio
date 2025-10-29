5c3ee4ac2d0d705c9a8367ff6d3adad70c8474a0
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 22:45:53 -0700

    Update singleton.py

--- 03_OOP_&_Design_Pattern/Library Management System/utils/singleton.py
+++ 03_OOP_&_Design_Pattern/Library Management System/utils/singleton.py
@@ -1,1 +1,17 @@
+"""
+singleton.py
+--------------
+Implements the Singleton design pattern.
+Ensures that only one Library instance exists in the entire program.
+"""
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """Return an existing instance or create a new one if it doesn’t exist."""
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
