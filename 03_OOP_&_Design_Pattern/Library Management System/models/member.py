9d90b1e803e8b887c6b5d32994bbb7316646559b
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 22:47:33 -0700

    Update member.py

--- 03_OOP_&_Design_Pattern/Library Management System/models/member.py
+++ 03_OOP_&_Design_Pattern/Library Management System/models/member.py
@@ -1,1 +1,26 @@
+"""
+member.py
+-----------
+Represents a library member (borrower).
+Keeps track of borrowed books and member info.
+"""
+
+from utils.id_generator import IDGenerator
+
+class Member:
+    def __init__(self, name):
+        self.member_id = IDGenerator.generate_id()
+        self.name = name
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        """Add book to borrowed list."""
+        self.borrowed_books.append(book)
+
+    def return_book(self, book):
+        """Remove book from borrowed list."""
+        self.borrowed_books.remove(book)
+
+    def __str__(self):
+        return f"{self.name} (Borrowed: {len(self.borrowed_books)} books)"
 
