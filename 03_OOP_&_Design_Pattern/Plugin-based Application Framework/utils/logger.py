cb09920a84db1ebf7b32330b6e89de038f031c0d
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 23:05:18 -0700

    Update logger.py

--- 03_OOP_&_Design_Pattern/Plugin-based Application Framework/utils/logger.py
+++ 03_OOP_&_Design_Pattern/Plugin-based Application Framework/utils/logger.py
@@ -1,1 +1,26 @@
+"""
+logger.py
+---------
+Tiny publish-style logger that other components can subscribe to.
+This is intentionally simple; in production you'd use structlog/logging.
+"""
+
+from typing import Callable, List
+
+
+class Logger:
+    _subscribers: List[Callable[[str], None]] = []
+
+    @classmethod
+    def subscribe(cls, handler: Callable[[str], None]) -> None:
+        cls._subscribers.append(handler)
+
+    @classmethod
+    def log(cls, message: str) -> None:
+        for h in list(cls._subscribers):
+            try:
+                h(message)
+            except Exception:
+                # swallow subscriber exceptions to keep logging robust
+                pass
 
