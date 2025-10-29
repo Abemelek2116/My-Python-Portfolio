a043e00458262e04e1dac0665b4043aee17957e5
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 22:46:14 -0700

    Update id_generator.py

--- 03_OOP_&_Design_Pattern/Library Management System/utils/id_generator.py
+++ 03_OOP_&_Design_Pattern/Library Management System/utils/id_generator.py
@@ -1,1 +1,14 @@
+"""
+id_generator.py
+----------------
+Generates unique IDs for books, members, and transactions.
+"""
+
+import uuid
+
+class IDGenerator:
+    @staticmethod
+    def generate_id():
+        """Generate a short unique ID."""
+        return str(uuid.uuid4())[:8]
 
