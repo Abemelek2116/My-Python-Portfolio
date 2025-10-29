c18762bae319068673f5c9f9bc9b96f25b6c9e68
Abemelek2116 <158573240+Abemelek2116@users.noreply.github.com>
2025-10-28 22:47:10 -0700

    Update book.py

--- 03_OOP_&_Design_Pattern/Library Management System/models/book.py
+++ 03_OOP_&_Design_Pattern/Library Management System/models/book.py
@@ -1,1 +1,30 @@
+"""
+book.py
+--------
+Represents a single book in the library system.
+Encapsulates details such as title, author, and availability status.
+"""
+
+from utils.id_generator import IDGenerator
+
+class Book:
+    def __init__(self, title, author):
+        self.book_id = IDGenerator.generate_id()
+        self.title = title
+        self.author = author
+        self.available = True
+
+    def borrow(self):
+        """Mark book as borrowed."""
+        if not self.available:
+            raise ValueError("Book is currently unavailable.")
+        self.available = False
+
+    def return_book(self):
+        """Mark book as available again."""
+        self.available = True
+
+    def __str__(self):
+        status = "Available" if self.available else "Borrowed"
+        return f"{self.title} by {self.author} ({status})"
 
